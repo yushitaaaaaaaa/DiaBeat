b841abfde1a8cfcd4eff04f7562fefff41871bad
yushitaaaaaaaa <142979933+yushitaaaaaaaa@users.noreply.github.com>
2025-01-15 13:53:54 +0530

    Update index.js

--- diabeat/src/index.js
+++ diabeat/src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {WorkoutsContextProvider} from './context/WorkoutContext'
+import {WorkoutsContextProvider} from './context/EntryContext'
 import {AuthContextProvider} from './context/AuthContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
-    <WorkoutsContextProvider>
+    <EntriesContextProvider>
       <App />
-    </WorkoutsContextProvider>
+    </EntriesContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
